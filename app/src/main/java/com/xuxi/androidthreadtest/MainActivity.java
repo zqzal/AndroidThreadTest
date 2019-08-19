afb5fb8f514ba4249d2b74c47915c35ba5d0f898
zqzal <936952020@qq.com>
2019-08-19 10:07:25 +0800

    android Handler Message

--- app/src/main/java/com/xuxi/androidthreadtest/MainActivity.java
+++ app/src/main/java/com/xuxi/androidthreadtest/MainActivity.java
@@ -3,13 +3,34 @@ package com.xuxi.androidthreadtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public static final int UPDATE_TEXT = 1;
+
     private TextView text;
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you");
+                    break;
+                    default:
+                        break;
+            }
+        }
+
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +50,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        text.setText("Nice To meet you");
+//                        text.setText("Nice To meet you");
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);//将Message对象发送出去
                     }
                 }).start();
                 break;
