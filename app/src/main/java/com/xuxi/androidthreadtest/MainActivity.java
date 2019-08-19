5ef3b2747d24ebe6cb6c39f3c9a3f7cdcd86dc29
zqzal <936952020@qq.com>
2019-08-19 09:54:20 +0800

    Only the original thread that created a view hierarchy can touch its views.

--- app/src/main/java/com/xuxi/androidthreadtest/MainActivity.java
+++ app/src/main/java/com/xuxi/androidthreadtest/MainActivity.java
@@ -3,12 +3,38 @@ package com.xuxi.androidthreadtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.text);
+        Button changeText = findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText("Nice To meet you");
+                    }
+                }).start();
+                break;
+                default:
+                    break;
+        }
     }
 }
